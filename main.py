aff66d6f98a4e8516729c56434db4ceba493944b
nexus106 <106nexus@gmail.com>
2023-12-15 16:51:43 +0900

    Add: 全学部、研究科のテーブル取得、csv作成

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ pd.set_option('display.max_rows', 150)
 url = 'https://www.ritsumei.ac.jp/profile/info/calendar/'
 dfs = pd.read_html(url)
 
-print(len(dfs))
-
-# for item in dfs:
-#     print(item)
-
+# 学部
 df_gakubu = pd.concat([dfs[0], dfs[1]], ignore_index=True)
+df_gakubu.to_csv("gakubu.csv", index=False)
 
-df_gakubu.to_csv("gakubu.csv")
+# 大学院文系研究科セメスター制
+df_daigakuin_bunkei_semester = pd.concat([dfs[2], dfs[3]], ignore_index=True)
+df_daigakuin_bunkei_semester.to_csv("daigakuin_semester_bunkei.csv", index=False)
+
+# 大学院理系セメスター制
+df_daigakuin_rikei_semester = pd.concat([dfs[4], dfs[5]], ignore_index=True)
+df_daigakuin_rikei_semester.to_csv("daigakuin_semester_rikei.csv", index=False)
+
+# 大学院クオーター制
+df_daigakuin_quarter = pd.concat([dfs[6], dfs[7]], ignore_index=True)
+df_daigakuin_quarter.to_csv("daigakuin_semester_quarter.csv", index=False)
+
+# 大学院法務研究科
+df_daigakuin_houmu = pd.concat([dfs[8], dfs[9]], ignore_index=True)
+df_daigakuin_houmu.to_csv("daigakuin_houmu.csv", index=False)
