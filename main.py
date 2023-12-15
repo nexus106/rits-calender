14c852f9b1b5d4ab8e64ad2bc22e75dc6a4e2960
nexus106 <106nexus@gmail.com>
2023-12-15 18:56:06 +0900

    Add: データフレームに年度を追加し、csvファイルで保存する関数を追加, Del: 今までのcsvファイルの名前を変更したかったので、前のcsvファイルは削除

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import pandas as pd
 
 
+def df_year_csv(df, name):
+    # 基本は2023年で、1月2月3月のときは2024年の列を追加
+    df['年'] = 2023
+    df.loc[df['月'].isin([1, 2, 3]), '年'] = 2024
+    # 順番を整理
+    df = df[['年', '月', '日', '曜', '行事']]
+    df.to_csv(name + ".csv", index=False)
+
+
 url = 'https://www.ritsumei.ac.jp/profile/info/calendar/'
 dfs = pd.read_html(url)
 
 # 学部
 df_gakubu = pd.concat([dfs[0], dfs[1]], ignore_index=True)
-df_gakubu.to_csv("gakubu.csv", index=False)
+df_year_csv(df_gakubu, "gakubu")
 
 # 大学院文系研究科セメスター制
-df_daigakuin_bunkei_semester = pd.concat([dfs[2], dfs[3]], ignore_index=True)
-df_daigakuin_bunkei_semester.to_csv("daigakuin_semester_bunkei.csv", index=False)
+df_daigakuin_bunkei = pd.concat([dfs[2], dfs[3]], ignore_index=True)
+df_year_csv(df_daigakuin_bunkei, "daigakuin_bunkei")
 
 # 大学院理系セメスター制
-df_daigakuin_rikei_semester = pd.concat([dfs[4], dfs[5]], ignore_index=True)
-df_daigakuin_rikei_semester.to_csv("daigakuin_semester_rikei.csv", index=False)
+df_daigakuin_rikei = pd.concat([dfs[4], dfs[5]], ignore_index=True)
+df_year_csv(df_daigakuin_rikei, "daigakuin_rikei")
 
 # 大学院クオーター制
 df_daigakuin_quarter = pd.concat([dfs[6], dfs[7]], ignore_index=True)
-df_daigakuin_quarter.to_csv("daigakuin_semester_quarter.csv", index=False)
+df_year_csv(df_daigakuin_quarter, "daigakuin_quarter")
 
 # 大学院法務研究科
 df_daigakuin_houmu = pd.concat([dfs[8], dfs[9]], ignore_index=True)
-df_daigakuin_houmu.to_csv("daigakuin_houmu.csv", index=False)
+df_year_csv(df_daigakuin_houmu, "daigakuin_houmu")
